713224dc23d2f10894c7346b460a4e15ce76f809
Owen Davison <odavison@cs.dal.ca>
2016-02-19 17:42:31 -0400

    Add more code to qt testing example

--- test/example_qttest.cpp
+++ test/example_qttest.cpp
@@ -1,10 +1,47 @@
 #include <QTest>
 
-class Example : public QObject {
-  Q_OBJECT
-    private slots:
-    void t1() { QVERIFY(true); }
+// Imports for units under test are relative to src/ directory
+#include "TArea.h"
+
+class TestQString : public QObject {
+    Q_OBJECT
+private slots:
+    void toUpper_qverify();
+    void toUpper_qcompare();
+    void toUpper_usingdata();
+    void toUpper_usingdata_data();
 };
 
-QTEST_MAIN(Example)
+
+void TestQString::toUpper_qverify()
+{
+  QString str = "Hello";
+  QVERIFY(str.toUpper() == "HELLO");
+}
+
+void TestQString::toUpper_qcompare()
+{
+  QString str = "Hello";
+  QCOMPARE(str.toUpper(), QString("HELLO"));
+}
+
+void TestQString::toUpper_usingdata()
+{
+  QFETCH(QString, string);
+  QFETCH(QString, result);
+
+  QCOMPARE(string.toUpper(), result);
+}
+
+void TestQString::toUpper_usingdata_data()
+{
+  QTest::addColumn<QString>("string");
+  QTest::addColumn<QString>("result");
+
+  QTest::newRow("all lower") << "hello" << "HELLO";
+  QTest::newRow("mixed")     << "Hello" << "HELLO";
+  QTest::newRow("all upper") << "HELLO" << "HELLO";
+}
+
+QTEST_MAIN(TestQString)
 #include "example_qttest.moc"
